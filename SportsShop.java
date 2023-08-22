e29528224d1ec0e7a48172e42c2fdf9f20f7861e
kavyadevaraj <kavyadevaraj.d1@gmail.com>
2023-08-22 22:31:29 +0530

    uploading array example

---
+++ SportsShop.java
@@ -0,0 +1,15 @@
+class SportsShop{
+   //10 Datatypes variableName[] ={value1,value2,....};
+   static String sportMaterial[] = {"Criket Bat","football","volleyBall","Hockey",
+   "leatherBoard","carrom Board","Trophies","Medals",
+   "Jersey","Badminton"};
+   
+     static int price[] = {1000,3000,1200,400,5000,300,450,670};
+      
+	  public static void main(String shop[]){
+	     System.out.println("The sport material is "+sportMaterial[0]+ 
+		 " "+ sportMaterial[1]+ " "+sportMaterial[2]+ " "+sportMaterial[3] );
+		 //-- price[0]
+		// System.out.println("The sport material price is "+ "--price[0]);
+		 }
+}
