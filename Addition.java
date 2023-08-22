964656a481d6014a28f8d216ffeb1f8fbd74af1b
kavyadevaraj <kavyadevaraj.d1@gmail.com>
2023-08-22 12:28:54 +0530

    addotion

---
+++ Addition.java
@@ -0,0 +1,62 @@
+/*class Addition{
+  public static void main(String add[]){
+   int total = 45+57;
+   int total1 =50+50;
+   int add3 = 69+90;
+   int add1 = 45+45;
+   int total2 =76+90;
+   int total3= 50+80;
+   int total4 = 40+403;
+   int total5 = 56+560;
+   int total6 = 600+400;
+   int total7 = 430+200;
+   int total8 = 44+64;
+   System.out.println("The total number is "+total);
+   System.out.println("The total number is "+total1);
+   System.out.println("The total number is "+add3);
+   System.out.println("The total number is "+add1);
+   System.out.println("The total number is "+total2);
+   System.out.println("The total number is "+total3);
+   System.out.println("The total number is "+total4);
+   System.out.println("The total number is "+total5);
+   System.out.println("The total number is "+total6);
+   System.out.println("The total number is "+total7);
+   System.out.println("The total number is "+total8);
+  }
+}
+
+   //method outside class declaration */
+   class Addition{
+   public static void main(String add[]){
+	   //invoking a method
+	   add(45,57);
+	   add(567,90);
+	   add(40,80);
+	   add(50,60);
+	   add(57,90);
+	   add(600,74);
+	   add(40,89);
+	   add(46,89);
+	   add(90,90);
+	   add(45,54);*/
+   }
+   public static void add(int a,int b){
+	  System.out.println(a+b);
+   }
+   }
+	  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+   
