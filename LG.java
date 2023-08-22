9a91b9944b1eba3c1517ece086887eb535f60ca1
kavyadevaraj <kavyadevaraj.d1@gmail.com>
2023-08-22 12:36:13 +0530

    uploading variable example

---
+++ LG.java
@@ -0,0 +1,14 @@
+class LG{
+   public static void main(String LG[]){
+     String washingMachineBrand ="LG";
+	 String machineFunctionType ="Fully Automatic Front Load";
+	 String machineCapacity ="8kg";
+	 String machineRPM ="1200rpm";
+	 int machineNumberofWashingPrograms =14;
+	 System.out.println("The washingMachineBrand name is "+washingMachineBrand);
+	 System.out.println("The MachineFuntiontype is "+machineFunctionType);
+	 System.out.println("The MachineCapacity is "+machineCapacity);
+	 System.out.println("The MachineRPM is "+machineRPM);
+	 System.out.println("The MachineNumberofWashingPrograms is "+machineNumberofWashingPrograms);
+   }
+}
